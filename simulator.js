1b78d8d42aac31274e455294913e0cd18d25c8d8
test <test@hotmail.com>
2017-08-03 17:56:08 +0800

    20170803

---
+++ simulator.js
@@ -0,0 +1,31 @@
+var net = require('net');
+var path = require('path');
+
+var PORT = 3000;
+var HOST = '127.0.0.1';
+
+// tcp服务端
+var server = net.createServer(function(socket){
+    console.log('服务端：收到来自客户端的请求');
+
+    socket.on('data', function(data){
+        console.log('服务端：收到客户端数据，内容为{'+ data.toString('hex') +'}');
+
+        // 给客户端返回数据
+        //socket.write('你好，我是服务端');
+    });
+
+    socket.on('close', function(){
+         console.log('服务端：客户端连接断开');
+    });
+});
+
+server.listen(path.join('\\\\?\\pipe', 'vessel-simulator'), function(){
+    console.log('服务端：开始监听来自客户端的请求');
+});
+
+
+
+
+
+
